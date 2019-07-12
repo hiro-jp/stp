936adf25209e92505356ee1d7754f4cc0a3e6dae
Hiroyuki Imai <hiroyuki@example.com>
2019-07-12 22:04:12 +0900

    ignore secret key

---
+++ stp/secret.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 's6tsv0(0i%b^!r(wx%&7h=#t@y)s)zbyxukq98dby@jac&okg#'
--- stp/settings.py
+++ stp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import stp.secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's6tsv0(0i%b^!r(wx%&7h=#t@y)s)zbyxukq98dby@jac&okg#'
+SECRET_KEY = stp.secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

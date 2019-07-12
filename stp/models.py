904e465cf06cb2d1d35510f44f631e2f0d96c531
Hiroyuki Imai <hiroyuki@example.com>
2019-07-12 21:58:33 +0900

    add basic model classes

--- stp/models.py
+++ stp/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+from users.models import User
+
 
 def get_or_create_item():
-    item, _ = Item.objects.get_or_create(name="no item")
+    item, _ = Item.objects.get_or_create(name="null item")
     return item.pk
+
+
+def get_or_create_order():
+    order, _ = Order.objects.get_or_create(pk=1)
+    return order.pk
+
+
+def get_or_create_user():
+    user, _ = User.objects.get_or_create(pk=1)
+    return user.pk
 
 
 class Item(models.Model):
@@ -19,3 +31,32 @@ class BasketItem(models.Model):
         on_delete=models.CASCADE,
         default=get_or_create_item,
     )
+    nos = models.IntegerField(
+        default=0,
+    )
+    order = models.ForeignKey(
+        "Order",
+        on_delete=models.CASCADE,
+        default=get_or_create_order,
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        default=get_or_create_user,
+    )
+
+
+class Order(models.Model):
+    pass
+
+
+class Dealer(models.Model):
+    pass
+
+
+class Packet(models.Model):
+    pass
+
+
+class Campaign(models.Model):
+    pass
--- stp/tests.py
+++ stp/tests.py
@@ -1,7 +1,10 @@
+from numbers import Number
+
 from django.test import TestCase
 from django.urls import reverse
 
-from stp.models import BasketItem, Item
+from stp.models import BasketItem, Item, Order, Dealer, Packet, Campaign
+from users.models import User
 
 
 class StpViewTest(TestCase):
@@ -12,21 +15,60 @@ class StpViewTest(TestCase):
 
 
 class StpModelTest(TestCase):
-    # BasketItem に要素を何も追加しなかったら、カウントは０
-    def test_basket_item_is_empty(self):
-        saved_basket_items = BasketItem.objects.all()
-        self.assertEqual(saved_basket_items.count(), 0)
-
     # BasketItem に要素を一つ追加したら、カウントは１
     def test_basket_item_is_not_empty(self):
+        before_save_count = BasketItem.objects.all().count()
         basket_item = BasketItem()
         basket_item.save()
         saved_basket_items = BasketItem.objects.all()
-        self.assertEqual(saved_basket_items.count(), 1)
+        self.assertEqual(saved_basket_items.count() - before_save_count, 1)
 
-    # BasketItem は item: Item(models.Model) を要素に持つ
-    def test_basket_item_has_item(self):
+    # Order に要素を一つ追加したら、カウントは１増加
+    def test_order_is_not_empty(self):
+        before_save_count = Order.objects.all().count()
+        order = Order()
+        order.save()
+        orders = Order.objects.all()
+        self.assertEqual(orders.count() - before_save_count, 1)
+
+    # Dealer に要素を一つ追加したら、カウントは１
+    def test_dealer_is_not_empty(self):
+        before_save_count = Dealer.objects.all().count()
+        dealer = Dealer()
+        dealer.save()
+        dealers = Dealer.objects.all()
+        self.assertEqual(dealers.count() - before_save_count, 1)
+
+    # Item に要素を一つ追加したら、カウントは１
+    def test_item_is_not_empty(self):
+        before_save_count = Item.objects.all().count()
+        item = Item()
+        item.save()
+        items = Item.objects.all()
+        self.assertEqual(items.count() - before_save_count, 1)
+
+    # Packet に要素を一つ追加したら、カウントは１
+    def test_packet_is_not_empty(self):
+        before_save_count = Packet.objects.all().count()
+        packet = Packet()
+        packet.save()
+        packets = Packet.objects.all()
+        self.assertEqual(packets.count() - before_save_count, 1)
+
+    # Campaign に要素を一つ追加したら、カウントは１
+    def test_campaign_is_not_empty(self):
+        before_save_count = Campaign.objects.all().count()
+        campaign = Campaign()
+        campaign.sage()
+        campaigns = Campaign.objects.all()
+        self.assertEqual(campaigns.count() - before_save_count, 1)
+
+    # BasketItem はいくつかの要素を持つ
+    def test_basket_item_has_some_attributes(self):
         basket_item = BasketItem()
         basket_item.save()
         first_basket_item = BasketItem.objects.first()
         self.assertIsInstance(first_basket_item.item, Item)
+        self.assertIsInstance(first_basket_item.nos, Number)
+        self.assertIsInstance(first_basket_item.order, Order)
+        self.assertIsInstance(first_basket_item.user, User)
